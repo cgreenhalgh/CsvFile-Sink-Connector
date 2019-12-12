a7dc4760ec60b722d99ce6a9ee716e9e7aac4e01
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2019-12-12 15:32:48 +0000

    unpack key

--- src/main/java/org/radarcns/connect/csvfile/CsvFileSinkTask.java
+++ src/main/java/org/radarcns/connect/csvfile/CsvFileSinkTask.java
@@ -24,6 +24,7 @@ import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.errors.IllegalWorkerStateException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
+import org.apache.kafka.connect.data.Struct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +40,11 @@ import java.util.concurrent.BlockingQueue;
  */
 public class CsvFileSinkTask extends SinkTask {
     private static final Logger log = LoggerFactory.getLogger(CsvFileSinkTask.class);
+    public static final String PROJECT_ID = "projectId";
+    public static final String USER_ID = "userId";
+    public static final String SOURCE_ID = "sourceId";
+    public static final String TIME = "time";
+    public static final String TIME_RECEIVED = "timeReceived";
 
     public CsvFileSinkTask() {
     }
@@ -57,10 +63,37 @@ public class CsvFileSinkTask extends SinkTask {
         for (SinkRecord record : sinkRecords) {
             TopicPartition partition = new TopicPartition(record.topic(),
                     record.kafkaPartition());
-            if (log.isDebugEnabled()) {
-                log.debug("{} --> {}", partition, record.kafkaOffset());
-            }
-	    // TODO ...
+	    if (! ( record.key() instanceof Struct ) ) {
+		log.warn("Key is not struct at {}.{}: {}: {}",
+				    partition, record.kafkaOffset(), record.key().getClass().getName(), record.key());
+		continue;
+	    }
+	    Struct key = (Struct)record.key();
+	    if (! ( record.value() instanceof Struct ) ) {
+		log.warn("Value is not struct at {}.{}: {}: {}",
+				  partition, record.kafkaOffset(), record.value().getClass().getName(), record.value());
+		continue;
+	    }
+ 	    Struct value = (Struct)record.value();
+	    try {
+ 		String projectId = key.getString(PROJECT_ID);
+		String userId = key.getString(USER_ID);
+		String sourceId = key.getString(SOURCE_ID);
+		log.debug("time is a {}", value.schema().field(TIME));
+		Double time = value.getFloat64(TIME);
+		// or timeCompleted (active)
+		Double timeReceived = value.schema().field(TIME_RECEIVED)!=null ? value.getFloat64(TIME_RECEIVED) : null;
+		log.info("{}.{}: project {} user {} source {} at time {} received {} : {}",
+				partition, record.kafkaOffset(),
+				projectId, userId, sourceId,
+				time, timeReceived, value);
+		// TODO
+	    } catch (Exception e) {
+		log.error("processing record {}.{}: {} -> {}: {}",
+				partition, record.kafkaOffset(), 
+				record.key(), record.value(), e);
+		log.error("Caused by", e);
+	    }
         }
     }
 
